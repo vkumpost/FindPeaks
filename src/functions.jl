d5fa35c09c4f6d21810663676acd850d9887d2e1
Vojtech Kumpost <vkumpost@gmail.com>
2021-05-27 19:30:51 +0200

    Add detailed comments in findidx function

--- src/functions.jl
+++ src/functions.jl
@@ -4,22 +4,36 @@
 Find indices of peaks in `data`. For flat peaks, return only the lowest index.
 """
 function findidx(data::Vector)
+
+    # Print warning if the data vector contains NaN values
     if any(isnan.(data))
         @warn "Data vector contains NaN values!"
     end
+
     n = length(data)  # length of data
-    idx = Int[]
-    idx_candidate = 0
+    idx = Int[]  # index vector for found peaks
+    idx_candidate = 0  # index of a potential peak
+
+    # iterate data points
     for i = 2:n-1
+
+        # if previous data point was smaller,
+        # the current index becomes a candidate
         if data[i-1] < data[i]
             idx_candidate = i
         end
+
+        # if a candidate exists and future data point is smaller, 
+        # then the point is a peak and its index saved
         if idx_candidate > 0 && data[i] > data[i+1]
             push!(idx, idx_candidate)
             idx_candidate = 0
         end
+
     end
+
     return idx
+
 end
 
 
