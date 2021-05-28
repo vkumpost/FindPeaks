b85195cb86137416edf20a8d11304f693c8e8b12
Vojtech Kumpost <vkumpost@gmail.com>
2021-05-28 23:39:53 +0200

    Add prominences and widths to plot recipe

--- src/Peaks.jl
+++ src/Peaks.jl
@@ -24,6 +24,25 @@ end
 
     idx = peaks.idx
     pks = peaks.pks
+    locs = peaks.locs
+    p = peaks.p
+    n = length(peaks)
+    wbounds = findwbounds(data, 1:length(data), idx, p)
+
+    for i = 1:n
+        @series begin
+            seriescolor := :orange
+            label := "Prominence"
+            primary := i == 1
+            [idx[i], idx[i]], [pks[i], pks[i]-p[i]]
+        end
+        @series begin
+            seriescolor := :gray
+            label := "Width"
+            primary := i == 1
+            [wbounds[i, 1], wbounds[i, 2]], [pks[i]-p[i]/2, pks[i]-p[i]/2]
+        end
+    end
 
     @series begin
         seriescolor := :black
@@ -35,6 +54,7 @@ end
         seriestype := :scatter
         seriescolor := :red
         label := "Peaks"
+        legend := false
         idx, pks
     end
 
