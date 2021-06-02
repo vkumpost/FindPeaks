414fdc06dd567683f0c145aa4e773fac0af5d06b
Vojtech Kumpost <vkumpost@gmail.com>
2021-06-03 00:12:49 +0200

    Add test for minprominence

--- test/test_functions.jl
+++ test/test_functions.jl
@@ -69,18 +69,33 @@ end
 
 end
 
-# @testset "findpeaks" begin
+@testset "findpeaks" begin
 
-#     data = [0, 1, 2, 3, 2, 1, 0, 1, 2, 2, 2, 0, 3, 3, 5, 4, 0]
-#     peaks = findpeaks(data)
-#     @test peaks isa Peaks
-#     @test peaks.pks == [3, 2, 5]
-#     @test peaks.locs == [4, 9, 15]
-#     @test peaks.w == [-1, -1, -1]
-#     @test all(isnan.(peaks.p))
+    @testset "minprominence" begin
 
-#     data = [1, 2, 3, 4, 5, 6, 6, 6, 6]
-#     peaks = findpeaks(data)
-#     @test isempty(peaks)
+        data = [0, 1, 2, 3, 2, 1, 0, 1, 2, 2, 2, 0, 3, 3, 6, 4, 0]
+        pr = findpeaks(data)
+        @test pr.indices == [4, 9, 15]
+        @test pr.prominences == [3, 2, 6]
+        @test all(pr.width_bounds .≈ [2.5 5.5; 8.0 11.5; 14.0 16.25])
 
-# end
+        pr = findpeaks(data; minprominence = 3)
+        @test pr.indices == [4, 15]
+        @test pr.prominences == [3, 6]
+        @test all(pr.width_bounds .≈ [2.5 5.5; 14.0 16.25])
+
+    end
+
+    # data = [0, 1, 2, 3, 2, 1, 0, 1, 2, 2, 2, 0, 3, 3, 5, 4, 0]
+    # peaks = findpeaks(data)
+    # @test peaks isa Peaks
+    # @test peaks.pks == [3, 2, 5]
+    # @test peaks.locs == [4, 9, 15]
+    # @test peaks.w == [-1, -1, -1]
+    # @test all(isnan.(peaks.p))
+
+    # data = [1, 2, 3, 4, 5, 6, 6, 6, 6]
+    # peaks = findpeaks(data)
+    # @test isempty(peaks)
+
+end
