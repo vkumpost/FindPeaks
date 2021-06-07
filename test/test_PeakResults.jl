7cfa143db669ba19ff08bd4b74b36653fc165b60
Vojtech Kumpost <vkumpost@gmail.com>
2021-06-07 22:04:34 +0200

    Add tests for PeakResults indexing

--- test/test_PeakResults.jl
+++ test/test_PeakResults.jl
@@ -18,6 +18,19 @@ end
     pr = PeakResults([], [], [], [], reshape([], 0, 2))
     @test isempty(pr)
     @test length(pr) == 0
+    
+    indices = [0, 2, 4, 6]
+    peaks = [1, 3, 5, 7]
+    locations = [10, 11, 12, 13]
+    prominences = [21, 22, 23, 24]
+    width_bounds = [1 1; 2 2; 3 3; 4 4]
+    pr = PeakResults(indices, peaks, locations, prominences, width_bounds)
+    pr_select = pr[[2, 4]]
+    @test pr_select.indices == [2, 6]
+    @test pr_select.peaks == [3, 7]
+    @test pr_select.locations == [11, 13]
+    @test pr_select.prominences == [22, 24]
+    @test pr_select.width_bounds == [2 2; 4 4]
 
 end
 
